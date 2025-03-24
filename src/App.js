13d609c5427484d25df7c4cd46b08e1233606c08
tcraig <taylrmcraig415@gmail.com>
2025-03-23 22:34:12 -0400

    Initial project setup with create-react-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TaskPlanner from './components/TaskPlanner';
+
+function App() {
+  return (
+    <div className="App">
+      <TaskPlanner />
+    </div>
+  );
+}
+
+export default App;
